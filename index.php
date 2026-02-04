260cdacc3cb0ec239b0455f99d072e0d4242be6a
drozdegor <drozdegor@mail.ru>
2026-02-04 10:30:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список страниц</title>
+</head>
+<body>
+    <h1>Список тестовых страниц</h1>
+    <ul>
+        <li><a href="page1.php">Страница 1 (404)</a></li>
+        <li><a href="page2.php?text=Пример текста для сохранения">Страница 2 (скачать файл)</a></li>
+        <li><a href="page3.php">Страница 3 (счётчик открытий)</a></li>
+        <li><a href="page4.php">Страница 4 (результат)</a></li>
+    </ul>
+</body>
+</html>
